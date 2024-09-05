848f59ca5bd760d59ddd787259e7064dd6776531
danmarquees <d.silvamarques@gmail.com>
2024-09-05 19:57:28 -0300

    updates

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from core.views import index
+from core import views
 
 app_name = "core"
 
 urlpatterns = [
-    path("", index)
+    path('blog/', views.blog, name='blog'),
+    path('blog-details/', views.blog_details, name='blog-details'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('', views.index, name='index'),
+    path('shop-details/', views.shop_details, name='shop-details'),
+    path('shop-grid/', views.shop_grid, name='shop-grid'),
+    path('shopping-cart/', views.shopping_cart, name='shopping-cart'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def blog(request):
+    return render(request, 'core/blog.html')
+
+def blog_details(request):
+    return render(request, 'core/blog-details.html')
+
+def checkout(request):
+    return render(request, 'core/checkout.html')
+
+def main(request):
+    return render(request, 'core/main.html')
+
+def shop_details(request):
+    return render(request, 'core/shop-details.html')
+
+def shop_grid(request):
+    return render(request, 'core/shop-grid.html')
+
+def shopping_cart(request):
+    return render(request, 'core/shopping-cart.html')
